3b1f3e022aa1b9040eec07ea4f811f3b1360b569
hanok <hanokgeorge.c@gmail.com>
2026-05-23 20:53:50 +0530

    completed todo project

---
+++ todo/todo.js
@@ -0,0 +1,72 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let todo = [];
+
+function tododisplay(){
+
+    console.log("\n==== TODO OPTIONS ====");
+    console.log("1. Add your todo");
+    console.log("2. Exit");
+    console.log("3.Delete");
+    console.log("4.see to do");
+    console.log("5.Edit the todolist");
+    
+    rl.question("Enter your option: ", (choice) => {
+
+        switch(choice){
+
+            case "1":
+
+                rl.question("Enter your task: ", (task) => {
+                    todo.push(task);
+                    console.log("Task added");
+                    console.log(todo);
+                    tododisplay();
+                });
+
+                break;
+
+            case "2":
+
+                console.log("Program exited");
+                 rl.close();
+                break;
+
+          case "3":
+            todo.pop();
+            console.log("last option deleted exited");
+            console.log(todo);
+            tododisplay();
+            break;
+
+            case"4":
+            console.log(todo);
+            console.log("this the existing todolist");
+            tododisplay();
+            break;
+
+            case "5":
+
+               rl.question("Enter your position: ", (number) => {
+               rl.question("Enter new value: ", (newtask) => {
+               todo[number - 1] = newtask;
+               console.log("Task updated");
+               console.log(todo);
+               tododisplay();
+
+        });
+
+    });
+
+    break;
+        }
+
+     } );
+
+}
+
+tododisplay();
